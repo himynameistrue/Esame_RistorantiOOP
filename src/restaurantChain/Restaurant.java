24d2754f0755c0d9f50ca23b6d5c8e5867f6a2b3
Vera <Vera@localhost>
2017-06-29 17:15:11 +0200

    R3 completed

--- src/restaurantChain/Restaurant.java
+++ src/restaurantChain/Restaurant.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Restaurant {
 	private String nome;
 	private int tavoli;
 	
-	private List<Menu> carta = new ArrayList<>();
+	private Map<String, Menu> carta = new HashMap<>();
 	private Map<String, Integer> prenotazioni = new HashMap<>();
 	private int personeRifiutate = 0;
 	private int tavoliOccupati = 0;
+	
+	private Map<String, List<Menu>> ordini = new HashMap<>();
+	private Map<String, Double> pagati = new HashMap<>();
 	
 	public Restaurant(String name, int tables){
 		this.nome = name;
@@ -25,10 +30,10 @@ public class Restaurant {
 	
 	public void addMenu(String name, double price) throws InvalidName{
 		Menu menu = new Menu(name, price);
-		if(carta.contains(menu)){
+		if(carta.containsKey(menu)){
 			throw new InvalidName("menu già presente");
 		}
-		carta.add(menu);
+		carta.put(name, menu);
 	}
 	
 	public int reserve(String name, int persons) throws InvalidName{
@@ -53,23 +58,65 @@ public class Restaurant {
 	}
 	
 	public boolean order(String name, String... menu) throws InvalidName{
-		return false;
+		if(!prenotazioni.containsKey(name)){
+			throw new InvalidName("Referente non presente");
+		}
+		int numeroOrdinazioni = 0;
+		for(String s : menu){
+			numeroOrdinazioni++;
+		}
+		if( numeroOrdinazioni < prenotazioni.get(name)){
+			return false;
+		}
+		List<Menu> listaOrdinazioni = new ArrayList<>();
+		for(String m : menu){
+			if(carta.get(m)== null){
+				throw new InvalidName("Menu non presente");
+			}
+			listaOrdinazioni.add(carta.get(m));
+		}
+		ordini.put(name, listaOrdinazioni);
+		return true;
 	}
 	
 	public List<String> getUnordered(){
-		return null;
+		List<String> nonOrdinati = new ArrayList<>();
+		for(String s : prenotazioni.keySet()){
+			if(ordini.get(s) == null){
+				nonOrdinati.add(s);
+			}
+		}
+		return nonOrdinati;
 	}
 	
 	public double pay(String name) throws InvalidName{
-		return -1.0;
+		if(!prenotazioni.containsKey(name)){
+			throw new InvalidName("Nessun tavolo riservato");
+		}
+		if(ordini.get(name) == null){
+			return 0;
+		}
+		double conto =  ordini.get(name).stream()
+				.collect(Collectors.summingDouble(Menu::getPrezzo));
+		pagati.put(name, conto);
+		return conto;
 	}
 	
 	public List<String> getUnpaid(){
-		return null;
+		List<String> nonPagati = new ArrayList<>();
+		for(String s : prenotazioni.keySet()){
+			if(!pagati.keySet().contains(s)){
+				nonPagati.add(s);
+			}
+		}
+		return nonPagati.stream()
+				.sorted()
+				.collect(Collectors.toList());
 	}
 	
 	public double getIncome(){
-		return -1.0;
+		return pagati.values().stream()
+				.reduce(0.0,(a, b) -> a + b);
 	}
 
 }
